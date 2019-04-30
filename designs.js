4b8073307a8a3f3cb39d3da2da8030857596e2b6
Rob Mayzley <rob.mayzley@gmail.com>
2019-04-30 09:56:52 +0100

    Added table creation function

--- designs.js
+++ designs.js
@@ -5,26 +5,47 @@ var colorInput = document.getElementById("colorPicker");
 // Select size input
 
 var sizePicker = document.getElementById("sizePicker");
-var pixelCanvas = document.getElementById("pixelCanvas");
 var submit = document.getElementById("submit");
+
+// Select table
+
+var table = document.getElementById("pixelCanvas")
+
+//Call values
 
 sizePicker.addEventListener("submit", function (form) {
     form.preventDefault();
     console.log('Form submit');
+    // Print values in console
     var height = document.getElementById("inputHeight").value;
     console.log(height);
     var width = document.getElementById("inputWidth").value;
     console.log(width);
     var color = document.getElementById("colorPicker").value;
     console.log(color);
+    // Call makeGrid to create table
     makeGrid();
 });
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
-// Your code goes here!
     console.log("makeGrid called");
-
+    // Clear table
+    while (table.hasChildNodes()) {
+        table.removeChild(table.lastChild);
+    }
+    // Get values
+    var height = document.getElementById("inputHeight").value;
+    var width = document.getElementById("inputWidth").value;
+    // Number of rows
+    for (var a = 0; a < height; a++){
+        var row = table.appendChild(document.createElement('tr'));
+        var columns = "";
+        // Number of columns
+        for (var b = 0; b < width; b++) {
+            columns += "<td></td>";
+        };
+        row.innerHTML = columns;
+    };
 };
