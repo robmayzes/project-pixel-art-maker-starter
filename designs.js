6db3de6b93b396c702d1a4800ec9d98ef8f6d51c
Rob Mayzes <hello@robmayzes.com>
2019-04-30 11:27:41 +0100

    Added more comments

--- designs.js
+++ designs.js
@@ -1,17 +1,17 @@
 // Select color input
 
-var colorInput = document.getElementById("colorPicker");
+var colorInput = document.getElementById("colorPicker"); // Color field
 
 // Select size input
 
-var sizePicker = document.getElementById("sizePicker");
-var submit = document.getElementById("submit");
+var sizePicker = document.getElementById("sizePicker"); // Both table size fields
+var submit = document.getElementById("submit"); // Submit button
 
 // Select table
 
-var table = document.getElementById("pixelCanvas")
+var table = document.getElementById("pixelCanvas") // Table
 
-//Call values
+// Update table when submit is clicked
 
 sizePicker.addEventListener("submit", function (form) {
     form.preventDefault();
@@ -19,7 +19,7 @@ sizePicker.addEventListener("submit", function (form) {
     makeGrid();
 });
 
-// When size is submitted by the user, call makeGrid()
+// Function to create the grid according to dimensions input by user
 
 function makeGrid() {
     // Clear table
@@ -27,21 +27,21 @@ function makeGrid() {
         table.removeChild(table.lastChild);
     }
     // Get values
-    var height = document.getElementById("inputHeight").value;
+    var height = document.getElementById("inputHeight").value; // Height form
     if (height > 30) {
         var height = 30;
     };
-    var width = document.getElementById("inputWidth").value;
+    var width = document.getElementById("inputWidth").value; // Width form
     if (width > 80) {
         var width = 80;
     };
     // Number of rows
     for (var a = 0; a < height; a++){
-        var row = table.appendChild(document.createElement('tr'));
+        var row = table.appendChild(document.createElement('tr')); // Add new rows
         // Number of columns
         for (var b = 0; b < width; b++) {
-            var column = row.appendChild(document.createElement('td'));
-            column.addEventListener('click', paintCell);
+            var column = row.appendChild(document.createElement('td')); // Add columns within row
+            column.addEventListener('click', paintCell); // Listen for clicks on each new cell
         };
     };
 };
@@ -49,6 +49,6 @@ function makeGrid() {
 // Funcion to paint cells when clicked
 
 function paintCell() {
-    var color = document.getElementById("colorPicker").value;
-    event.target.style.backgroundColor = color;
+    var color = document.getElementById("colorPicker").value; // Get color from color picker
+    event.target.style.backgroundColor = color; // Change background color of clicked cell
 }
