2285ab07c4824e8a235e2e6e35abbfe0a390aea4
Rob Mayzley <rob.mayzley@gmail.com>
2019-04-30 09:17:44 +0100

    Added event listener

--- designs.js
+++ designs.js
@@ -4,17 +4,27 @@ var colorInput = document.getElementById("colorPicker");
 
 // Select size input
 
-var sizeInput = document.getElementById("sizePicker");
+var sizePicker = document.getElementById("sizePicker");
 var pixelCanvas = document.getElementById("pixelCanvas");
 var submit = document.getElementById("submit");
+
+sizePicker.addEventListener("submit", function (form) {
+    form.preventDefault();
+    console.log('Form submit');
+    var height = document.getElementById("inputHeight").value;
+    console.log(height);
+    var width = document.getElementById("inputWidth").value;
+    console.log(width);
+    var color = document.getElementById("colorPicker").value;
+    console.log(color);
+    makeGrid();
+});
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
 
 // Your code goes here!
+    console.log("makeGrid called");
 
-
-
-
-}
+};
