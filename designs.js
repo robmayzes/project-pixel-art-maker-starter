b5a511a67f1e618a4a3449da1b3533a9bcf7566e
Rob Mayzley <rob.mayzley@gmail.com>
2019-04-30 08:57:05 +0100

    Added variables and identifiers

--- designs.js
+++ designs.js
@@ -1,10 +1,20 @@
 // Select color input
+
+var colorInput = document.getElementById("colorPicker");
+
 // Select size input
+
+var sizeInput = document.getElementById("sizePicker");
+var pixelCanvas = document.getElementById("pixelCanvas");
+var submit = document.getElementById("submit");
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
 
 // Your code goes here!
+
+
+
 
 }
